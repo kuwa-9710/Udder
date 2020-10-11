1c6d37d7cbf27adc42319635085afc0996c92f7a
Tomoya Kuwashima <apple.941.s.j@gmail.com>
2020-10-11 21:46:10 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Tomoya Kuwashima on 2020/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
